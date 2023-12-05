4fce0010b6736d64118a7984de2eb262cae4d9d2
José Domínguez P <jdominguez@viamatica.me>
2023-12-05 17:27:54 -0500

    feat(root): 🎉 Init cron-ticker-app

---
+++ 01.cron-ticker/main.js
@@ -0,0 +1,11 @@
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+  times++;
+
+  console.log('Tick cada 5 segundos', times);
+});
+
+console.info('Inicio de la tarea cron');
